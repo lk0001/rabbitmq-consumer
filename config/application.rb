7de5fd6ef3c141324a29bea342a951043f0a5f80
Łukasz Kornek <lk0001@wp.pl>
2015-04-08 12:51:11 +0200

    #9 setup sneakers rake tasks

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module RabbitmqConsumer
   class Application < Rails::Application
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,5 +24,7 @@ module RabbitmqConsumer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.active_job.queue_adapter = :sneakers
   end
 end
